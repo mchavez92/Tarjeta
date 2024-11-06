9a6a7f701d0374b014bd5ea5cbc1a47d8c2b80d5
Martin Chavez <martin.chavez0492@gmail.com>
2024-11-06 02:28:07 -0500

    Tarjeta hecha

---
+++ script.js
@@ -0,0 +1,26 @@
+/*function confirmarCita1() {
+ 
+    const fecha = "06 de noviembre del 2024";
+    const hora = "19:00"; // Puedes cambiar esta hora fija o hacerlo dinámico si prefieres
+    const url = `confirmacion.html?lugar=${encodeURIComponent(lugar)}&fecha=${encodeURIComponent(fecha)}&hora=${encodeURIComponent(hora)}`;
+    window.location.href = url;
+}*/
+
+function confirmarCita2() {
+ 
+/*    const fecha = "06 de noviembre del 2024";
+    const hora = "19:00"; // Puedes cambiar esta hora fija o hacerlo dinámico si prefieres
+    const url = `confirmacion.html?lugar=${encodeURIComponent(lugar)}&fecha=${encodeURIComponent(fecha)}&hora=${encodeURIComponent(hora)}`;
+    window.location.href = url;*/
+
+    window.location.href = "confirmacion.html";
+
+}
+
+/*function confirmarCita3() {
+ 
+    const fecha = "06 de noviembre del 2024";
+    const hora = "19:00"; // Puedes cambiar esta hora fija o hacerlo dinámico si prefieres
+    const url = `confirmacion.html?lugar=${encodeURIComponent(lugar)}&fecha=${encodeURIComponent(fecha)}&hora=${encodeURIComponent(hora)}`;
+    window.location.href = url;
+}*/
